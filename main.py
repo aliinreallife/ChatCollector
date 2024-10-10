ea743c2421dd24c54907f45f7866adcdce68fac4
ali <aliinreallifee@gmail.com>
2024-10-10 19:38:04 +0330

    validate the link inside workflow before starting ubunto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import re
-import sys
 from utils import (
     deduplicate_messages,
     extract_messages,
@@ -21,25 +20,34 @@ def main():
 
     sharable_link = args.share_link
 
-    if not validate_link(sharable_link):
-        print(f"Invalid share link format: {sharable_link}")
-        sys.exit(1)
-
-    # Extract the part after the last "/" from the sharable link to use as the filename
-    output_filename = f"{sharable_link.split('/')[-1]}.md"
-
     content = fetch_content_from_url(sharable_link)
 
-    pattern = r'"message":\{.*?"author":"(.*?)".*?"create_time":([0-9.]+).*?"parts":\s*\[(.*?)\]'
+    if content is None or len(content) == 0:
+        print(f"Failed to fetch content from the link: {sharable_link}")
+        return False
+
+    # Regular expression to extract messages
+    pattern = r'"message":\{.*?"author":\{"role":"(.*?)".*?"create_time":([0-9.]+).*?"parts":\s*\[(.*?)\]'
 
     # Extract and process the data
     matches = extract_messages(content, pattern)
+
+    if not matches:
+        print(f"Failed to extract messages from the content: {sharable_link}")
+        return False
+
     deduplicated_data = deduplicate_messages(matches)
     sorted_data = sort_messages_by_time(deduplicated_data)
+
+    output_filename = f"{sharable_link.split('/')[-1]}.md"
+
     output_file_path = write_to_md_file(sorted_data, output_filename)
 
     print(f"Sorted and deduplicated content has been saved to {output_file_path}")
+    return True
 
 
 if __name__ == "__main__":
-    main()
+    success = main()
+    if not success:
+        print("The process failed.")
