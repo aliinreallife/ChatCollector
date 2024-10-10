04623947e0441d706e422edc757bbdf84d9da891
ali <aliinreallifee@gmail.com>
2024-10-10 20:12:25 +0330

    blahblah

--- main.py
+++ main.py
@@ -22,26 +22,31 @@ def main():
     sharable_link = args.share_link
 
     if not validate_link(sharable_link):
-        print(f"Invalid share link format: {sharable_link}", file=sys.stderr)
+        print(f"Invalid share link format: {sharable_link}")
         return False
 
     try:
         content = fetch_content_from_url(sharable_link)
     except Exception as e:
-        print(f"Error fetching content from the link: {sharable_link}. Error: {e}", file=sys.stderr)
+        print(
+            f"Error fetching content from the link: {sharable_link}. Error: {e}",
+        )
         return False
 
     if content is None or len(content) == 0:
-        print(f"Failed to fetch or empty content from the link: {sharable_link}", file=sys.stderr)
+        print(
+            f"Failed to fetch or empty content from the link: {sharable_link}",
+        )
         return False
-
-    pattern = r'"message":\{.*?"author":"(.*?)".*?"create_time":([0-9.]+).*?"parts":\s*\[(.*?)\]'
+    pattern = r'"message":\{.*?"author":\{"role":"(.*?)".*?"create_time":([0-9.]+).*?"parts":\s*\[(.*?)\]'
 
     # Extract and process the data
     matches = extract_messages(content, pattern)
 
     if not matches:
-        print(f"Failed to extract messages from the content: {sharable_link}", file=sys.stderr)
+        print(
+            f"Failed to extract messages from the content: {sharable_link}",
+        )
         return False
 
     deduplicated_data = deduplicate_messages(matches)
@@ -52,7 +57,9 @@ def main():
     try:
         output_file_path = write_to_md_file(sorted_data, output_filename)
     except Exception as e:
-        print(f"Error writing content to file: {output_filename}. Error: {e}", file=sys.stderr)
+        print(
+            f"Error writing content to file: {output_filename}. Error: {e}",
+        )
         return False
 
     print(f"Sorted and deduplicated content has been saved to {output_file_path}")
